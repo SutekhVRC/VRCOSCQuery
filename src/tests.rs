7674af1f4c2695e29c57f7c0dfe7aedd4ffeb71d
Sutekh <101658846+SutekhVRC@users.noreply.github.com>
2023-11-19 16:35:53 -0800

    thanks

---
+++ src/tests.rs
@@ -0,0 +1,13 @@
+use super::*;
+
+#[test]
+fn it_works() {
+    let result = add(2, 2);
+    assert_eq!(result, 4);
+}
+
+#[test]
+fn lol() {
+    let mjau = add(69, 1337);
+    assert_eq!(mjau, 69+1337);
+}
