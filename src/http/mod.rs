0939c63870b21d2ad1fcb05ddf4ff62585a83dd7
Sutekh <101658846+SutekhVRC@users.noreply.github.com>
2023-11-20 18:47:31 -0800

    Working http module and async runtime

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,6 +1,76 @@
-use std::net::TcpListener;
+use std::{sync::atomic::{AtomicBool, Ordering}, net::{Ipv4Addr, SocketAddrV4}, time::Duration, io};
 
+use log::info;
+use tokio::{sync::watch::Receiver, net::{TcpListener, TcpStream}};
 
-pub struct OQHTTPHandler {
+use crate::http::host_info::HostInfo;
+
+pub mod host_info;
+
+const HTTP_RESPONSE_BASE: &'static str = "HTTP/1.1 200\r\nContent-Type: application/json\r\n";
+
+pub struct OQHTTPHandler <'hostinfo>{
+        thread_rx: Receiver<AtomicBool>,
+        bound_addr: Option<SocketAddrV4>,
+        host_info: HostInfo<'hostinfo>,
+}
+
+impl <'hostinfo> OQHTTPHandler<'hostinfo> {
     
+    pub fn new(bind_addr: SocketAddrV4, host_info: HostInfo<'hostinfo>, thread_rx: Receiver<AtomicBool>) -> Self {
+        OQHTTPHandler { thread_rx, bound_addr: Some(bind_addr), host_info }
+    }
+
+    pub async fn handle_connection(&self, tcp_stream: &TcpStream) {
+
+        let mut full_buffer = Vec::new();
+        let mut buffer: [u8; 1024] = [0x0; 1024];
+        tcp_stream.readable().await.unwrap();
+
+        loop {
+
+            match tcp_stream.try_read(&mut buffer) {
+                Ok(0) => break,
+                Ok(_b) => {
+
+                    full_buffer.extend_from_slice(&buffer);
+                    if _b < 1024 {break};
+                    buffer.fill_with(||0x0);
+                },
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
+                Err(_e) => break,
+            }
+        }
+
+        //println!("-=== Buffer ===-\n{}", String::from_utf8_lossy(&full_buffer).to_owned());
+
+        let h_info = serde_json::to_string(&self.host_info).unwrap();
+
+        let http_res = format!("{}Content-Length: {}\r\n\r\n{}", HTTP_RESPONSE_BASE, h_info.len(), h_info);
+
+        println!("-== SENDING ==-\n{}", http_res);
+
+        let bytes_sent = tcp_stream.try_write(http_res.as_bytes()).unwrap();
+        println!("Sent {} bytes", bytes_sent);
+    }
+}
+
+pub async fn start(http_handler: OQHTTPHandler<'_>) {
+        
+    let tcp_listener = TcpListener::bind(http_handler.bound_addr.unwrap()).await.unwrap();
+
+    let mut state = true;
+
+    while state {
+        println!("while start");
+        if let Ok((s, a)) = tcp_listener.accept().await {
+            println!("Got connection from: {}", a);
+            http_handler.handle_connection(&s).await;
+            println!("Handler done!");
+        }
+
+        if http_handler.thread_rx.has_changed().unwrap() {
+            state = http_handler.thread_rx.borrow().load(Ordering::SeqCst);
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
-use std::net::SocketAddrV4;
+use std::net::{SocketAddrV4, Ipv4Addr};
+use std::sync::atomic::AtomicBool;
+use std::time::Duration;
 
+use log::info;
 use tokio::runtime::Runtime;
+//use tokio
+use tokio::sync::watch;
+
+use crate::http::OQHTTPHandler;
+use crate::http::host_info::{HostInfo, HostInfoExtensions};
 
 mod mdns;
 mod http;
@@ -12,6 +20,8 @@ mod tests;
 pub struct OSCQuery {
     http_net: SocketAddrV4,
     async_runtime: Option<Runtime>,
+    thread_tx: Option<watch::Sender<AtomicBool>>,
+    thread_rx: Option<watch::Receiver<AtomicBool>>,
 }
 
 impl OSCQuery {
@@ -19,19 +29,67 @@ impl OSCQuery {
         OSCQuery {
             http_net,
             async_runtime: None,
+            thread_tx: None,
+            thread_rx: None,
         }
     }
 
     pub fn start_oq(&mut self) {
+
         
+        let (thread_tx, thread_rx) = watch::channel::<AtomicBool>(AtomicBool::new(false));
+        self.thread_tx = Some(thread_tx);
+        self.thread_rx = Some(thread_rx);
+
         self.async_runtime = Some(Runtime::new().unwrap());
+        info!("Started Async runtime..");
 
         self.start_http();
+        self.start_mdns();
+    }
+
+    pub fn stop_oq(&mut self) {
+
+        let tx = self.thread_tx.take().unwrap();
+        tx.send(AtomicBool::new(false)).unwrap();
+        
+        info!("Sent shutdown signal to OSCQuery threads..");
+
+        info!("Shutting down async runtime..");
+        self.async_runtime.take().unwrap().shutdown_timeout(Duration::from_secs(10));
+        info!("Async runtime successfully shutdown.");
 
     }
 
     fn start_http(&mut self) {
 
-        
+        info!("Staring HTTP service..");
+
+        let extensions = HostInfoExtensions {
+            access: true,
+            clipmode: false,
+            range: true,
+            _type: true,
+            value: true,
+        };
+
+        let host_info = HostInfo {
+            name: "VRCOSCQuery Test",
+            extensions,
+            osc_ip: "127.0.0.1",
+            osc_port: 8080,
+            osc_transport: "UDP",
+        };
+
+        let http_service = OQHTTPHandler::new(self.http_net, host_info, self.thread_rx.clone().unwrap());
+        let http_thread = crate::http::start(http_service);
+        self.async_runtime.as_mut().unwrap().spawn(http_thread);
+
+        info!("HTTP service running..");
+    }
+
+    fn start_mdns(&mut self) {
+
+
     }
 }
