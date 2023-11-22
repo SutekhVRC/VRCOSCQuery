9708fdf5cca74fbadbef517a71caa73728e228a2
Sutekh <101658846+SutekhVRC@users.noreply.github.com>
2023-11-22 15:40:04 -0800

    New test and change old test to force discovery test

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -25,7 +25,7 @@ fn instantiate_10_seconds() {
 }
 
 #[test]
-fn instantiate_forever() {
+fn test_vrchat_force_discover() {
     let osc_addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 9069);
     let http_addr = SocketAddrV4::new(Ipv4Addr::new(172, 19, 19, 244), 8080);
 
@@ -35,7 +35,19 @@ fn instantiate_forever() {
 
     loop {
         //instance.register_mdns_service();
+        //instance.unregister_mdns_service();
         instance.attempt_force_vrc_response_detect();
-        std::thread::sleep(Duration::from_secs(1));
+        std::thread::sleep(Duration::from_secs(10));
     }
+}
+
+#[test]
+fn detect_vrchat() {
+    
+    let osc_addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 9069);
+    let http_addr = SocketAddrV4::new(Ipv4Addr::new(172, 19, 19, 244), 8080);
+
+    let mut instance = OSCQuery::new("VibeCheck".to_string(), http_addr, osc_addr);
+    let resolved_vrc_service = instance.mdns_search("VRChat-Client-".to_string(), OSC_JSON_SERVICE);
+    info!("[+] Got VRChat OSC JSON: {}", resolved_vrc_service.get_hostname());
 }
