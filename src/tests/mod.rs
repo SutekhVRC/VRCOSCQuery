88bd355f74b89cdf7a39ad0599c64370d7077c0c
Sutekh <101658846+SutekhVRC@users.noreply.github.com>
2023-11-22 04:01:43 -0800

    Test using new interface

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -5,13 +5,26 @@ use super::*;
 #[test]
 fn instantiate_30_seconds() {
 
-    let osc_addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 9079);
+    let osc_addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 9069);
     let http_addr = SocketAddrV4::new(Ipv4Addr::new(172, 19, 19, 244), 8080);
 
-    let mut instance = OSCQuery::new("BLOOP".to_string(), http_addr, osc_addr);
-    instance.start_oq();
-    instance.mdns_search("VRChat-Client-".to_string());
+    let mut instance = OSCQuery::new("VibeCheck".to_string(), http_addr, osc_addr);
+    instance.start_http_json();
+    instance.register_mdns_service();
+
+    loop {
+    let _resolved_vrc_service = instance.mdns_search("VRChat-Client-".to_string());
+    std::thread::sleep(Duration::from_secs(1));
+    }
+    //info!("[+] Got resolved VRC Service: {:?}", resolved_vrc_service);
+
     std::thread::sleep(Duration::from_secs(30));
-    info!("Sending stop signal!");
-    instance.stop_oq();
+
+    info!("[*] Stopping JSON service..");
+    instance.stop_http_json();
+    info!("[+] JSON service shutdown.");
+
+    info!("[*] Stopping mDNS listener..");
+    instance.unregister_mdns_service();
+    info!("[+] mDNS listener shutdown.");
 }
